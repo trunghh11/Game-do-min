19019266afb44495b6dc20734bd06fe4bbbe8307
trung2910 <trung291004@gmail.com>
2023-03-05 16:56:15 +0700

    delete gamedomin.exe and update gamedomin.cpp with custom map

--- Gamedomin.cpp
+++ Gamedomin.cpp
@@ -10,72 +10,106 @@ using namespace std;
 const char boom = '#';
 const char nothing = 'O';
 
-void sleep(int seconds) {
-    this_thread::sleep_for(chrono:: seconds((seconds)));
-}
-void print(char** a, int n) {
-    cout << "\t\t\t\tYOUR MAP\n\n";
-    for (int i = 0; i < n ; i++) {
+void print(char** a, int m , int n) {
+    cout << "\t\t\t\tHERE IS YOUR MAP:\n\n";
+    for (int i = 0; i < m ; i++) {
         cout << "\t\t\t\t";
         for (int j = 0; j < n; j++) {
-            cout << a[i][j] << " ";
+            if (a[i][j] == boom) {
+                cout << "\033[31m" << a[i][j] << "\033[0m" << " ";
+            }
+            else
+                cout << a[i][j] << " ";
         }
         cout << endl;
     }
-    cout << "\t\t  ==================================\n\n";
+    cout << "\n";
+    cout << "==================================\n\n";
+}
+
+int count_index(char** a,int m, int n ,int i, int j) {
+    int count = 0;
+    for (int k = i-1; k <= i+1; k++) {
+        for (int l = j-1; l <= j+1; l++) {
+            if (k >=0 && l >= 0 && k < m && k < n){
+                if (a[k][l] == boom) count++;
+                else {
+                    continue;
+                }
+            }
+            else continue;
+        }
+    }
+    return count;
+}
+
+void render_map_with_booms (char** b, int m, int n ) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            char count_ch = (count_index(b,m,n,i,j)) + 48;
+            if (b[i][j] != boom) b[i][j] = count_ch;
+        }
+    }
 }
 
 int main() {
     bool start = true;
     while (start == true) {
         system(CLEAR_COMMAND);
-        int n;
-        cout <<  "\t\t***** WELLCOME TO MY MINESWEEPER GAME *****\n\n";
-        cout << "\t\t\t\tCHOOSE YOUR MAP \n";
-        cout << "\t\t\t      ==================\n";
-        cout << "\t\t\t\t[1] - MAP 3x3 \n";
-        cout << "\t\t\t\t[2] - MAP 4x4\n";
-        cout << "\t\t\t\t[3] - MAP 5x5\n";
-        cout << "\t\t\t      ==================\n\n";
-        cout << "..::Enter your selection: ";
-        cin >> n;
-        while (n != 1 && n != 2 && n != 3) {
+        int m,n;
+        cout <<  "\t\t     ***** WELLCOME TO MY MINESWEEPER GAME *****\n\n";
+        cout << "\t\t\t\tLET'S CUSTOM YOUR MAP ! \n";
+        cout << "\t\t\t      ==========================\n\n";
+        cout << "..::Enter your Map [m x n]: ";
+        cin >> m >> n;
+        while (m <= 1 || n <= 1) {
             cout << '\n';
-            cout << "ERROR!! PLEASE TRY AGAIN: ";
-            cin >> n;
+            cout << "ERROR!! [m > 1 and n > 1] PLEASE TRY AGAIN: ";
+            cin >> m >> n;
         }
-        n+=2;
-        char** a = new char* [n];
-        char** b = new char* [n];
-        for (int i = 0; i < n; i++) {
+        int num_booms;
+        cout << "..::Enter the number of bombs you want [ < " << m*n << " ] : ";
+        cin >> num_booms;
+        while (num_booms >= m*n) {
+            cout << '\n';
+            cout << "ERROR!! [ bombs < " << m*n << " ] PLEASE ENTER  TRY AGAIN: ";
+            cin >> num_booms;
+        }
+
+        while (num_booms <= 0) {
+            cout << '\n';
+            cout << "ERROR!! [ bombs > 0 ] PLEASE ENTER  TRY AGAIN: ";
+            cin >> num_booms;
+        }
+
+        char** a = new char* [m];
+        char** b = new char* [m];
+        for (int i = 0; i < m; i++) {
             a[i] = new char [n];
             b[i] = new char [n];
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++){
-                b[i][j] = nothing;
-            }
-        }
+        
         srand(time(NULL));
         int x_rand,y_rand;
-        for (int i = 0; i < n-1; i++) {
-            x_rand = rand() % n;
+        for (int i = 0; i < num_booms; i++) {
+            x_rand = rand() % m;
             y_rand = rand() % n;
             while (b[x_rand][y_rand] == boom) {
-                x_rand = rand() % n;
+                x_rand = rand() % m;
                 y_rand = rand() % n;
             }
             b[x_rand][y_rand] = boom;
         }
 
+        render_map_with_booms(b,m,n);
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++){
                 a[i][j] = '*';
             }
         }
         system(CLEAR_COMMAND);
-        print(a,n);
+        print(a,m,n);
         bool gameOver = false, passGame = false;
         int count = 0;
         while (gameOver == false && passGame == false){
@@ -91,7 +125,7 @@ int main() {
             a[x][y] = b[x][y];
             system(CLEAR_COMMAND);
             if (a[x][y] == boom) {
-                print(b,n);
+                print(b,m,n);
                 cout << "GAME OVER!!!!!! \n";
                 cout << "--------------------------------\n";
                 char check;
@@ -106,21 +140,25 @@ int main() {
                 }
                 if ( check == 'y') {
                     gameOver = true;
+                    delete [] a;
+                    delete [] b;
                 }
                 else {
                     gameOver = true;
                     start = false;
+                    delete [] a;
+                    delete [] b;
                     return 0;
                 }
             }
             else {
-                print(a,n);
-                if (count < n*n - n){
+                print(a,m,n);
+                if (count < m*n - num_booms){
                     a[x][y] = b[x][y];
                     continue;
                 }
                 else {
-                    system(CLEAR_COMMAND);     
+                    system(CLEAR_COMMAND);
                     cout << "\t\t***** WINNER WINNER, CHICKEN DINER *****\n\n";
                     cout << "..:PLAY AGAIN? Y - YES ; N - NO\n";
                     char check;
@@ -134,10 +172,14 @@ int main() {
                     }
                     if ( check == 'y') {
                         passGame = true;
+                        delete [] a;
+                        delete [] b;
                     }
                     else {
                         passGame = true;
                         start = false;
+                        delete [] a;
+                        delete [] b;
                         return 0;
                     }
                 }
